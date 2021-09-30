a15d1eee1b9d64aeda9632a6602e212cab031374
Álef Vieira Coutinho <78700687+alefvieira@users.noreply.github.com>
2021-09-30 03:06:22 -0300

    erro corrigido

--- chamandopaghtml.py
+++ chamandopaghtml.py
@@ -6,7 +6,7 @@ pagina=open("index.html","w", encoding="UTF-8")
 pagina.write("""<!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
+    <!--<meta charset="UTF-8">-->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Previsão do Tempo</title>
