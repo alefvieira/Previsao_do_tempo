806560dd359268c47aad0ec9b89d177fdca151dc
Álef Vieira Coutinho <alef_coutin@hotmail.com>
2021-10-04 14:36:54 -0300

    ativanda o servidor

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
-# from flask import Flask, render_template
-# import chamandopaghtml as run_pag
-# import APP_Previsao_Tempo as AppPT
+from flask import Flask, render_template
+import chamandopaghtml as run_pag
+import APP_Previsao_Tempo as AppPT
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route("/")
+@app.route("/")
 
-# def index():
-#     AppPT.Dados_Capitais()
-#     Gera_Index() # VAI GERAR A PÁGINA HTML
-#     return render_template('index.html')
+def index():
+    AppPT.Dados_Capitais()
+    Gera_Index() # VAI GERAR A PÁGINA HTML
+    return render_template('index.html')
 
-# @app.route('/graficos')
-# def graficos():
+@app.route('/graficos')
+def graficos():
     
-#     return render_template('secao_graficos.html')
+    return render_template('secao_graficos.html')
 
 
-# # if __name__ == "__main__":
-# #     from waitress import serve
-# #     serve(app, host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
 
 # app.run()
 
