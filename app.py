3d8d35c99a4cba92053a56ed8488e0a69ee091d9
Álef Vieira Coutinho <alef_coutin@hotmail.com>
2021-10-04 11:55:02 -0300

    a

--- app.py
+++ app.py
@@ -19,7 +19,9 @@ def graficos():
     return render_template('secao_graficos.html')
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
 
 
 
